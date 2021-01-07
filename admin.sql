c9562d34d677817e1e577d24e758b46479ffd4e8
erce <canerce99@hotmail.com>
2021-01-07 17:19:09 +0300

    heroku

--- admin.sql
+++ admin.sql
@@ -8,11 +8,11 @@ INSERT INTO club_admins (nickname,password_hash)
 VALUES ('ituacm','$pbkdf2-sha256$29000$xfgfY4wR4vzfO6e0dq51Tg$5NMULXhDk3ZALE1Stem6AZsdgkEqvPL91G3kBYWFhd4');
 
 INSERT INTO club_admins (nickname,password_hash) 
-VALUES ('ituieee','$pbkdf2-sha256$29000$GmOs1frfm5OyFuK8dy5l7A$nriRyrsCZbWQ1AcFG0jijno15CR8cRl4LDHTbZ6egjw')
+VALUES ('ituieee','$pbkdf2-sha256$29000$GmOs1frfm5OyFuK8dy5l7A$nriRyrsCZbWQ1AcFG0jijno15CR8cRl4LDHTbZ6egjw');
 
 
 INSERT INTO club_managers (admin_id, club_id)
-VALUES (1,1)
+VALUES (1,1);
 
 INSERT INTO club_managers (admin_id, club_id)
-VALUES (2,3)
+VALUES (2,3);

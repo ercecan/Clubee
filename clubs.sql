512edb0669ef3868cfdf3b8f07f892b6295f804f
erce <canerce99@hotmail.com>
2021-01-13 02:01:58 +0300

    heroku

--- clubs.sql
+++ clubs.sql
@@ -5,17 +5,17 @@
 */
 
 INSERT INTO clubs (name,description,history,student_count,source,mission,vision,image_url) 
-VALUES ('ACM Student Chapter','description', 'history',12,'https://ituacm.com/','mission','vision','../static/images/itu_acm.png');
+VALUES ('ACM Student Chapter','description', 'history',0,'https://ituacm.com/','mission','vision','../static/images/itu_acm.png');
 
 INSERT INTO clubs (name,description,history,student_count,source,mission,vision,image_url) 
-VALUES ('Blockchain Technologies Club','description', 'history',12,'https://blockchain.itu.edu.tr/','mission','vision','../static/images/itu_acm.png');
+VALUES ('Blockchain Technologies Club','description', 'history',0,'https://blockchain.itu.edu.tr/','mission','vision','../static/images/itu_bc.png');
 
 INSERT INTO clubs (name,description,history,student_count,source,mission,vision,image_url) 
-VALUES ('IEEE Student Branch','description', 'history',12,'http://www.ituieee.com/','mission','vision','../static/images/itu_acm.png');
+VALUES ('IEEE Student Branch','description', 'history',0,'http://www.ituieee.com/','mission','vision','../static/images/itu_ieee.png');
 
 INSERT INTO clubs (name,description,history,student_count,source,mission,vision,image_url) 
-VALUES ('Data Science Club','description', 'history',12,'https://www.linkedin.com/company/itudss/','mission','vision','../static/images/itu_acm.png');
+VALUES ('Data Science Club','description', 'history',0,'https://www.linkedin.com/company/itudss/','mission','vision','../static/images/itu_ds.png');
 
 INSERT INTO clubs (name,description,history,student_count,source,mission,vision,image_url) 
-VALUES ('Robotics Club','description', 'history',12,'https://www.iturobotik.com/','mission','vision','../static/images/itu_acm.png');
+VALUES ('Robotics Club','description', 'history',0,'https://www.iturobotik.com/','mission','vision','../static/images/itu_rob.png');
 
